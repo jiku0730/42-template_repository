903822dd1a399e32cc791601ce6614acb19508bb
jiku0730 <kei2003730@gmail.com>
2025-10-29 10:07:46 +0900

    modified: HeaderfileにStdio.hを追加した

--- includes/project-name.h
+++ includes/project-name.h
@@ -6,13 +6,14 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 16:38:39 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/09/30 16:41:08 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:07:09 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROJECT_NAME_H
 # define PROJECT_NAME_H
 
-# include "../libft/libft.h"
+# include "libft.h"
+# include <stdio.h>
 
 #endif
